e0d830c5778047268091c07ddabb969cf0cb2074
aymanash <aiman.ashri@khind.com>
2026-04-17 09:44:57 +0800

    fix: redirect to forecast after login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,7 +33,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('forecast', absolute: false));
+        return redirect()->route('forecast');
     }
 
     /**
